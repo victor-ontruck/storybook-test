a7eb9b98f017730075e78adbf49b2de125a43bab
Victor Gil <victor.gil@ontruck.com>
2021-07-15 12:43:37 +0200

    feat(init): initial commit

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import ontruckTheme from "./ontruckTheme";
+
+addons.setConfig({
+  theme: ontruckTheme,
+});
---
+++ .storybook/ontruckTheme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Ontruck storybook",
+  brandUrl: "https://example.com",
+  brandImage:
+    "https://storage.googleapis.com/public_static_assets/ontruck_logo.png",
+});
